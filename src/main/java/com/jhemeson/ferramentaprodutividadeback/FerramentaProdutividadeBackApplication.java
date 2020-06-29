01c312757e32a348b0d4c211bd38ea3b600234f0
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-06-29 11:53:00 -0300

    initial commit

---
+++ src/main/java/com/jhemeson/ferramentaprodutividadeback/FerramentaProdutividadeBackApplication.java
@@ -0,0 +1,13 @@
+package com.jhemeson.ferramentaprodutividadeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerramentaProdutividadeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerramentaProdutividadeBackApplication.class, args);
+	}
+
+}
