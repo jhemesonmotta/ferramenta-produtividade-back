e22d7600e1bf05308911949094f0ebbd5b93c6af
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-06-29 17:05:39 -0300

    add mapper

--- src/main/java/com/jhemeson/ferramentaprodutividadeback/controller/BookController.java
+++ src/main/java/com/jhemeson/ferramentaprodutividadeback/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.jhemeson.ferramentaprodutividadeback.controller;
 
+import com.jhemeson.ferramentaprodutividadeback.dto.BookDTO;
 import com.jhemeson.ferramentaprodutividadeback.dto.MessageResponseDTO;
 import com.jhemeson.ferramentaprodutividadeback.entity.Author;
 import com.jhemeson.ferramentaprodutividadeback.entity.Book;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -23,7 +26,7 @@ public class BookController {
     }
 
     @PostMapping
-    public MessageResponseDTO create(@RequestBody Book book) {
-        return bookService.create(book);
+    public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
+        return bookService.create(bookDTO);
     }
 }
--- src/main/java/com/jhemeson/ferramentaprodutividadeback/service/BookService.java
+++ src/main/java/com/jhemeson/ferramentaprodutividadeback/service/BookService.java
@@ -1,24 +1,28 @@
 package com.jhemeson.ferramentaprodutividadeback.service;
 
+import com.jhemeson.ferramentaprodutividadeback.dto.BookDTO;
 import com.jhemeson.ferramentaprodutividadeback.dto.MessageResponseDTO;
 import com.jhemeson.ferramentaprodutividadeback.entity.Book;
+import com.jhemeson.ferramentaprodutividadeback.mapper.BookMapper;
 import com.jhemeson.ferramentaprodutividadeback.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class BookService {
 
     private BookRepository bookRepository;
+    private final BookMapper bookMapper = BookMapper.INSTANCE;
 
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public MessageResponseDTO create(Book book) {
-        Book savedBook = bookRepository.save(book);
+    public MessageResponseDTO create(BookDTO bookDTO) {
+        Book bookToSave = bookMapper.toModel(bookDTO);
+
+        Book savedBook = bookRepository.save(bookToSave);
         return MessageResponseDTO.builder().
                 message("Book created with id: " + savedBook.getId())
                 .build();
