0a0e86761bfd2d790400b11caa4aecb0a09596b9
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-06-29 15:19:58 -0300

    add lombok

--- src/main/java/com/jhemeson/ferramentaprodutividadeback/entity/Author.java
+++ src/main/java/com/jhemeson/ferramentaprodutividadeback/entity/Author.java
@@ -1,8 +1,17 @@
 package com.jhemeson.ferramentaprodutividadeback.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
