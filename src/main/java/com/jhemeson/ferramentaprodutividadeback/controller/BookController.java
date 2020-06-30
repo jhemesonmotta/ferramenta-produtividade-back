657c8a307e56a48f73808c8096969cc45e896785
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-06-30 14:44:33 -0300

    serviços para add e buscar por id

--- src/main/java/com/jhemeson/ferramentaprodutividadeback/controller/BookController.java
+++ src/main/java/com/jhemeson/ferramentaprodutividadeback/controller/BookController.java
@@ -2,10 +2,7 @@ package com.jhemeson.ferramentaprodutividadeback.controller;
 
 import com.jhemeson.ferramentaprodutividadeback.dto.BookDTO;
 import com.jhemeson.ferramentaprodutividadeback.dto.MessageResponseDTO;
-import com.jhemeson.ferramentaprodutividadeback.entity.Author;
-import com.jhemeson.ferramentaprodutividadeback.entity.Book;
 import com.jhemeson.ferramentaprodutividadeback.exception.BookNotFoundException;
-import com.jhemeson.ferramentaprodutividadeback.repository.BookRepository;
 import com.jhemeson.ferramentaprodutividadeback.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/jhemeson/ferramentaprodutividadeback/controller/ProjetoController.java
+++ src/main/java/com/jhemeson/ferramentaprodutividadeback/controller/ProjetoController.java
@@ -1,18 +1,38 @@
 package com.jhemeson.ferramentaprodutividadeback.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jhemeson.ferramentaprodutividadeback.dto.MessageResponseDTO;
+import com.jhemeson.ferramentaprodutividadeback.dto.ProjetoDTO;
+import com.jhemeson.ferramentaprodutividadeback.service.ProjetoService;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/projetos")
 public class ProjetoController {
+
+    private ProjetoService projetoService;
+
+    @Autowired
+    public ProjetoController(ProjetoService projetoService) {
+        this.projetoService = projetoService;
+    }
+
     // listar
 
     // encontrar por id
+    @GetMapping("/{id}")
+    public ProjetoDTO findById(@PathVariable Long id) throws NotFoundException {
+        return projetoService.findById(id);
+    }
 
     // deletar
 
     // adicionar
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody ProjetoDTO projetoDTO) {
+        return projetoService.create(projetoDTO);
+    }
 
     // atualizar
 }
